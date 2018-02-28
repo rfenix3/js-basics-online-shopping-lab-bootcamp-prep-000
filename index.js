c52ec5ea945a692a682502837c03ffc8ae185d3b
Richard Fenix <richard.fenix@gmail.com>
2018-02-28 00:09:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function placeOrder(cardNumber) {
     return;
   }
   console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`);
-  //cart = [];
+  cart = [];
 }
 
 viewCart();
